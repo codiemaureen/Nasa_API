7be72585990931c1eebe6d7d38b6ed8191c93ca1
Codiemaureen <codiestover.cs@gmail.com>
2024-07-22 17:01:27 -0400

    Adding Enviroment variables

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let hidden = true;
 function getFetch(){
   const date = document.getElementById("userDate").value;
   console.log(date);
-  const url = `https://api.nasa.gov/planetary/apod?api_key=cW3MjyR23t5ybWlIRARhHdvE0pohUf0SXUO1gYuM&date=${date}`
+  const url = `https://api.nasa.gov/planetary/apod?api_key=75Bnr6NAMZMjHVO0hCfFGUC9ECGK03DAP8iI5usM&date=${date}`
   console.log(hidden);
  hidden = !hidden;
   fetch(url)
