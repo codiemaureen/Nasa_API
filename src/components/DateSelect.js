3b76306d3df08d3e40d1d735e3f1c3a0a3f15d89
Codiemaureen <codiestover.cs@gmail.com>
2024-08-13 15:18:20 -0400

    Adding DateSelect Component

---
+++ src/components/DateSelect.js
@@ -0,0 +1,10 @@
+const DateSelect = () => {
+  return (
+    <div>
+      <input type="date" id="userDate" name="" value=""></input>
+      <button type="button" name="button">Get Today's Photo</button>
+    </div>
+  )
+}
+
+export default DateSelect
