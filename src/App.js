d91f25f0e011204d4dbac4208f5c8fa29119e977
Codiemaureen <codiestover.cs@gmail.com>
2024-08-13 15:17:57 -0400

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from './components/Header';
 import './style.css';
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import DateSelect from "./DateSelect"
+
 const Header = () => {
   return (
     <div>
       <h1>Nasa's Photo of the Day</h1>
+      <DateSelect />
     </div>
   )
 }
