685c6671c31584841f30382be51b6426441c881c
Codiemaureen <codiestover.cs@gmail.com>
2024-08-13 15:21:37 -0400

    Adding Tab title and icon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Header from './components/Header';
 import './style.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9378/rocket">Rocket</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+    </div>
+  )
+}
+
+export default Footer
