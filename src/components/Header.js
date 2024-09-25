4e7878ca804d751e0de35687108677f606fd9768
Codiemaureen <codiestover.cs@gmail.com>
2024-09-25 15:55:23 -0400

    Adding header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
-import DateSelect from "./DateSelect"
 import '../style/header.css'
 
 const Header = () => {
   return (
     <div>
       <h1>Nasa's Photo of the Day</h1>
-      <DateSelect />
     </div>
   )
 }
