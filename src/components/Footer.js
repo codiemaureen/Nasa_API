5451de42de4a86a86b4913caa1557306c7f130f3
Codiemaureen <codiestover.cs@gmail.com>
2024-09-27 09:49:23 -0400

    Fixing footer to bottom center of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
+import '../style/footer.css'
+
 const Footer = () => {
   return (
-    <div>
-      <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9378/rocket">Rocket</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
-    </div>
+    <footer>
+      <div className="codieCredit">
+        <span>Website built by <a href="https://codiemaureen.netlify.app/">Codie Maureen</a></span>
+      </div>
+      <div className="iconCredit">
+        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9378/rocket">Rocket</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+      </div> 
+    </footer>
   )
 }
 
