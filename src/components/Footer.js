8bd76a8fb28e36cd7d6f270c679ab3018360b62c
Codiemaureen <codiestover.cs@gmail.com>
2025-03-31 13:30:57 -0400

    Adding style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import '../style/footer.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='footerContainer'>
       <div className="codieCredit">
         <span>Website built by <a href="https://codiemaureen.netlify.app/">Codie Groth</a></span>
       </div>
       <div className="iconCredit">
-        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9378/rocket">Rocket</a> icon by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
+        <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/9378/rocket">Rocket Icon</a>  by <a target="_blank" rel="noreferrer" href="https://icons8.com">Icons8</a>
       </div> 
     </footer>
   )
